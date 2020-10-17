eedd1769789d61e1783cebaebb08aaabeaec22be
Diegomrza <2993323220101@ingenieria.usac.edu.gt>
2020-10-17 15:21:09 -0600

    .
    .

--- Login.py
+++ Login.py
@@ -1,16 +1,27 @@
 class Usuario:
 
     def __init__(self, id, nombre, apellido, usuario, password):
+    	self.id = id
     	self.nombre = nombre
     	self.apellido = apellido
     	self.usuario = usuario
     	self.password = password
 
     def muestra(self, usuario, password):
+
+
     	if self.usuario == usuario and self.password == password:
-    		print("Bienvenido")
+    		
+    		return True
     	else:
-    		print("Error")
+    		return False
+
+    def dump(self):
+    
+    	return {
+    	'id' :self.id,
+    	'nombre' :self.usuario
+    	}		
 
 
 usuario1 = Usuario(0, "Diego","Robles","Squery",'123')
--- app.py
+++ app.py
@@ -1,19 +1,42 @@
-from flask import Flask 
+from flask import Flask, request, jsonify
 from Login import Usuario
 
 
 usuarios = []
 usuarios.append(Usuario(1, "Diego", "Robles", "Squartul", "123"))
-usuarios.append(Usuario(2,"Angel", "Ávila", "Rangolfa","123"))
+usuarios.append(Usuario(2,"Angel", "Avila", "Rangolfa","123"))
 
 
 app = Flask(__name__)
 
+@app.route('/login', methods=['POST'])
+#Método para hacer la autenticación del usuario
+def login():
+	if request.method == 'POST':
+
+		response = {}
+
+		nombre = request.form.get('nombre_usuario')
+		password = request.form.get('password_usuario')
+
+		#For que recorre el arreglo de los usuarios
+		for user in usuarios:
+
+			#Devuelve True si encontró uno
+			if user.muestra(nombre,password) == True:
+
+				response["id"] = user.id
+				response["usuario"] = user.usuario
+
+				return response
+
+		#Devuelve False si no encontró un usuario
+		return "False"
+
+
 @app.route("/")
 def index():
-	return "<h1>" + "Hola amorrrr, att: tu nobio" + "</h1>"
-
-
+	return "<h1>Prueba de pagina en la nube</h1>"
 
 if __name__ == "__main__":
 	app.run(threaded = True,port = 5000, debug = True)
